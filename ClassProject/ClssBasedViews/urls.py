5e8f7e2abdbab2935a6c16339c9751deb748d9e5
Roshni V R <vrroshni2001@gmail.com>
2022-11-03 13:47:01 +0530

    Tried listView,DetailView

--- ClassProject/ClssBasedViews/urls.py
+++ ClassProject/ClssBasedViews/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from . import views
+from .views import *
 urlpatterns = [
-    path('',views.taskList,name='tasks'),
+    path('',TaskList.as_view(),name='tasks'),
+    path('task/<int:pk>/',TaskDetail.as_view(),name='task'),
 ]
--- ClassProject/ClssBasedViews/views.py
+++ ClassProject/ClssBasedViews/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import *
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 # Create your views here.
-def taskList(request):
-    return HttpResponse('Hello')
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+    template_name = 'ClssBasedViews/tasks.html'
 
+
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'ClssBasedViews/task.html'
+
+    
