28161223c375f7bdbbc5e7eb70359d2434dfa4fd
Roshni V R <vrroshni2001@gmail.com>
2022-11-03 12:50:46 +0530

    models added

--- ClassProject/ClssBasedViews/urls.py
+++ ClassProject/ClssBasedViews/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
-from django.views.generic import TemplateView
 urlpatterns = [
-    path('',TemplateView.as_view(template_name="index.html",extra_context={'title':'Custom Title'}))
+    path('',views.taskList,name='tasks'),
 ]
